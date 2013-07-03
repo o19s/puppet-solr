a861e0037c2a83bfbe715d4b87d394e3d9bf5580
root <root@domU-12-31-39-09-C4-18.compute-1.internal>
2013-07-03 10:01:29 -0400

    cleanup from testing

--- manifests/tomcat6.pp
+++ manifests/tomcat6.pp
@@ -19,27 +19,26 @@ class solr::tomcat6(
   $tomcat6_version = "6.0.37"
 ){
 
-  class solr {
-    ensure => present
+  class { "solr":
   }
 
-  exec {
+  Exec {
     path => $exec_path
   }
 
   exec { "wget tomcat":
     command => "wget --output-document=/tmp/apache-tomcat-${tomcat6_version}.tgz http://apache.petsads.us/tomcat/tomcat-6/v${tomcat6_version}/bin/apache-tomcat-${tomcat6_version}.tar.gz",
-    creates => "${tomcat6_home}/tomcat-${tomcate_version}"
+    creates => "${tomcat6_home}/tomcat-${tomcat6_version}"
   } ->
 
   exec { "untar tomcat":
-    command => "tar -xzf /tmp/apache-tomcat-${tomcat6_version}.tgz -C ${tomcat_home}",
-    creates => "${tomcat6_home}/tomcat-${tomcate_version}"
+    command => "tar -xzf /tmp/apache-tomcat-${tomcat6_version}.tgz -C ${tomcat6_home}",
+    creates => "${tomcat6_home}/tomcat-${tomcat6_version}"
   } ->
 
   file { "${tomcat6_home}/tomcat6":
     ensure => link,
-    target => "/${tomcat6_home}/apache-tomcat-${tomcat_version}",
+    target => "/${tomcat6_home}/apache-tomcat-${tomcat6_version}",
     owner  => solr
   }
 
@@ -51,15 +50,15 @@ class solr::tomcat6(
 
   # prep required solr libs for tomcat
   exec { "cp /opt/solr/example/lib/ext/* ${tomcat6_home}/tomcat6/lib/":
-    path => "/bin"
-    creates => "${tomcat6_home}/tomcat6/lib/log4j-1.2.16.jar"
-    require => Class['solr']
+    path => "/bin",
+    creates => "${tomcat6_home}/tomcat6/lib/log4j-1.2.16.jar",
+    require => [ Class['solr'], File["${tomcat6_home}/tomcat6"]]
   }
 
   # stage the solr war file for tomcat
   exec { "cp /opt/solr/dist/solr-4.3.0.war /etc/solr/solr.war":
-    path => "/bin"
-    creates => "/etc/solr/solr.war"
+    path => "/bin",
+    creates => "/etc/solr/solr.war",
     require => Exec["cp /opt/solr/example/lib/ext/* ${tomcat6_home}/tomcat6/lib/"],
   }
 
